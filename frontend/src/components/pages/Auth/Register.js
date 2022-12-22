9f5439c55e3c690e1d857f890c9a36e40dd4c169
Ronaldo Rios Espíndola <ronaldoriosespindola@gmail.com>
2022-12-22 12:17:05 -0300

    Resposivity device

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,9 +1,37 @@
+import Input from './Input'
+import styles from './Form.module.css'
+import {Link} from 'react-router-dom'
 
 function Register() {
 
+    function handleChange(event) {
+
+    }
+
+
     return (
-        <section>
-            <h1>Register</h1>
+        <section className={styles.form_container}>
+            <h1>Registrar</h1>
+
+            <form>
+                <Input text="Nome" type="text" name="name"
+                    placeholder="Digite o seu nome" handleOnChange={handleChange} />
+
+                <Input text="Telefone" type="tel" name="phone"
+                    placeholder="Digite o seu telefone" handleOnChange={handleChange} />
+
+                <Input text="E-mail" type="email" name="email"
+                    placeholder="Digite o seu melhor e-mail" handleOnChange={handleChange} />
+
+                <Input text="Senha" type="password" name="password"
+                    placeholder="Digite a sua senha" handleOnChange={handleChange} />
+
+                <Input text="Confirmar Senha" type="password" name="confirmpassword"
+                    placeholder="Digite novamente a sua senha" handleOnChange={handleChange} />
+
+                <input type="submit" value="Cadastrar" />
+            </form>
+            <p>Já tem conta? <Link to="/login">Clique aqui</Link></p>
         </section>
     )
 }
