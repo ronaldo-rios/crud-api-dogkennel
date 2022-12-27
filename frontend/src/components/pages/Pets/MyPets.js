82512e4e865801badbc2e9e71c78e23921e86028
Ronaldo Rios Espíndola <ronaldoriosespindola@gmail.com>
2022-12-27 12:29:24 -0300

    Creating crud function remove a pet

--- frontend/src/components/pages/Pets/MyPets.js
+++ frontend/src/components/pages/Pets/MyPets.js
@@ -5,7 +5,7 @@ import useFlashMessage from '../../../hooks/useFlashMessage'
 import api from '../../../utils/api'
 import styles from './Dashboard.module.css'
 
-// Router and fnction to view all user pets:
+// Router and function to view all user pets created:
 function MyPets() {
 
     const [pets, setPets] = useState([])
@@ -23,33 +23,57 @@ function MyPets() {
         })
     }, [token])
 
+
+
+    // async function to delete a pet:
+    async function removePet(id) {
+
+        let msgType = 'sucess'
+
+        const data = await api.delete(`pets/${id}`, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`
+            }
+        }).then((response) => {
+            const updatedPets = pets.filter((pet) => pet._id !== id)
+            setPets(updatedPets)
+            return response.data
+        }).catch((error) => {
+            msgType = 'error'
+            return error.response.data
+        })
+
+        setFlashMessage(data.message, msgType)
+    }
+
     return (
         <section>
-            <div>
-                <h1>MyPets</h1>
+            <div className={styles.petslist_header}>
+                <h1>Meus Pets Cadastrados</h1>
                 <Link to="/pets/add">Cadastrar Pet</Link>
             </div>
 
-            <div>
+            <div className={styles.petslist_container}>
                 {pets.length > 0 && (
                     pets.map((pet) => (
-                        <div key={pet._id}>
+                        <div key={pet._id} className={styles.petlist_row}>
                             <RoundedImage
                                 src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
                                 alt={pet.name}
                                 width="px75"
                             />
                             <span>{pet.name}</span>
-                            <div className={styles.actions}></div>
-
+                            <div className={styles.actions}>
                             {pet.available ? (
                                 <>
-                                    {pet.adopter && (<button>Concluir adoção</button>)}
+                                    {pet.adopter && (<button className={styles.conclude_btn}>Concluir adoção</button>)}
                                     <Link to={`/pets/edit/${pet._id}`}>Editar</Link>
-                                    <button>Remover</button>
+                                    <button onClick={() => {removePet(pet._id)}}>Remover</button>
                                 </>) : (
                                 <><p>Pet já adotado</p></>
                             )}
+                            </div>
+
                         </div>
                     ))
                 )}
