3d649d4582f8008deef2511d56a724d4ce245445
Ronaldo Rios Espíndola <ronaldoriosespindola@gmail.com>
2022-12-27 19:46:21 -0300

    Creating home page adoption to pets

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -16,8 +16,27 @@ function Home() {
 
     return (
         <section>
-            <div>
+            <div className={styles.pet_home_header}>
                 <h1>Adote Amor. Adote um Pet!</h1>
+            </div>
+            <div className={styles.pet_container}>
+                {pets.length > 0 && 
+                    pets.map((pet) => (
+                        <div className={styles.pet_card}>
+                            <div className={styles.pet_card_image} 
+                            style={{backgroundImage: `url(${process.env.REACT_APP_API}images/pets/${pet.images[0]})`}}></div>
+                            <h3>{pet.name}</h3>
+                            <p>
+                                <span>{`Peso: ${pet.weight} kg `}</span>
+                            </p>
+                            {pet.available ? (
+                                <Link to={`/pets/${pet._id}`}>Mais Detalhes</Link>
+                            ) : (
+                                <p className={styles.adopted_text}>Adotado!</p>
+                            )}
+                        </div>
+                    ))}
+                {pets.length === 0 && (<p>Não há pets cadastrados ou disponíveis para adoção no momento</p>)}
             </div>
         </section>
     )
